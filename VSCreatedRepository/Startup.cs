c27a6c1747c1ce0ca805753b97e10a76f5da2ba5
MelissaERCosta <melissa_e_costa@yahoo.com>
2016-07-20 19:43:06 -0400

    Another commit after changes have been made.

--- VSCreatedRepository/Startup.cs
+++ VSCreatedRepository/Startup.cs
@@ -4,6 +4,7 @@ using Owin;
 [assembly: OwinStartupAttribute(typeof(VSCreatedRepository.Startup))]
 namespace VSCreatedRepository
 {
+    /* This is a comment */
     public partial class Startup
     {
         public void Configuration(IAppBuilder app)
