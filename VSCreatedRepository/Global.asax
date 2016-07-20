be5a2448ba83ec9e4654af7e81ef60ee213e0922
MelissaERCosta <melissa_e_costa@yahoo.com>
2016-07-20 19:39:31 -0400

    Add project files.

---
+++ VSCreatedRepository/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VSCreatedRepository.MvcApplication" Language="C#" %>
---
+++ VSCreatedRepository/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VSCreatedRepository.Startup))]
+namespace VSCreatedRepository
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
